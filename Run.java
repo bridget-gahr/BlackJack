e7a911e12c2e42ba2d2b0bde3c942a6538f39264
bridget-gahr <gahrbridget@gmail.com>
2025-09-12 10:04:08 -0700

    addded players

--- Run.java
+++ Run.java
@@ -1,10 +1,32 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Run {
     public static void main(String[] args) {
         Deck deck = new Deck();
+        ArrayList<Player> players = new ArrayList<>();
 
         for(Card card : deck.getCards())
         {
             System.out.println(card);
         }
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How many players do you want: ");
+        int num_players = scanner.nextInt();
+        scanner.nextLine();
+
+        for(int i = 0; i < num_players; i++)
+        {
+            System.out.println("What is player " + (i + 1) + "'s name: ");
+            String name = scanner.nextLine();
+            players.add(new Player(name));
+        }
+        
+        System.out.println(players);
+
+        deck.shuffle;
+        
     }
 }
